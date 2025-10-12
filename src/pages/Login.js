b16f389c9adcbac02f9bf241db964d9e5788be3d
kennarattendance23 <Moonlitguardian23@gmail.com>
2025-10-12 23:21:52 +0800

    Initial commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -14,7 +14,7 @@ function Login({ onLoginChange }) {
   const handleLogin = async (e) => {
     e.preventDefault();
     try {
-      const res = await fetch('http://localhost:5000/api/login', {
+      const res = await fetch('https://kennarbackend.onrender.com/api/login', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, password }),
